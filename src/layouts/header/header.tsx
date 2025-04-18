1db61546319b0e78e431b102d85e8bf24e25eb2c
Amil Musaevi <amilmusaevi00@gmail.com>
2025-04-18 13:24:36 +0400

    created info sections image part

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,31 +1,31 @@
 import { useEffect } from "react";
-import { Link, Events, scrollSpy } from 'react-scroll';
+import { Link, Events, scrollSpy } from "react-scroll";
 import styles from "./header.module.scss";
 
-
 const Header = () => {
-    const navItems = [
-        { id: '1', label: 'Beranda' },
-        { id: '2', label: 'Profil' },
-        { id: '3', label: 'Tentang' },
-        { id: '4', label: 'Pertanyaan' },
-      ];
-     useEffect(() => {
-        Events.scrollEvent.register('begin', () => {});
-        Events.scrollEvent.register('end', () => {});
-        scrollSpy.update();
-        
-        return () => {
-          Events.scrollEvent.remove('begin');
-          Events.scrollEvent.remove('end');
-        };
-      }, []);
-  return( <div className={styles.header}>
-  <h2 className={styles.header_title}>Reborn</h2>
-  
-  <nav className="nav">
+  const navItems = [
+    { id: "1", label: "Beranda" },
+    { id: "2", label: "Profil" },
+    { id: "3", label: "Tentang" },
+    { id: "4", label: "Pertanyaan" },
+  ];
+  useEffect(() => {
+    Events.scrollEvent.register("begin", () => {});
+    Events.scrollEvent.register("end", () => {});
+    scrollSpy.update();
+
+    return () => {
+      Events.scrollEvent.remove("begin");
+      Events.scrollEvent.remove("end");
+    };
+  }, []);
+  return (
+    <div className={styles.header}>
+      <h2 className={styles.header_title}>Reborn</h2>
+
+      <nav className="nav">
         <ul>
-          {navItems.map(item => (
+          {navItems.map((item) => (
             <li key={item.id}>
               <Link
                 to={item.id}
@@ -40,7 +40,8 @@ const Header = () => {
           ))}
         </ul>
       </nav>
-  </div>)
+    </div>
+  );
 };
 
 export default Header;
